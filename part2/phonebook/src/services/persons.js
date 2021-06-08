929b6bc4a75fa6066ff8070ae80825b36a61989f
Naveen <74000045+NaveenMulaga@users.noreply.github.com>
2021-06-08 09:36:59 +0530

    part-2 Phonebook exercises

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+const createData = (data) => {
+  return axios.post(baseUrl, data).then((response) => response.data);
+};
+
+const deleteData = (id) => {
+  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
+};
+
+const updateData = (id, newData) => {
+  return axios
+    .put(`${baseUrl}/${id}`, newData)
+    .then((response) => response.data);
+};
+
+const logger = {
+  getAll,
+  createData,
+  deleteData,
+  updateData,
+};
+export default logger;
